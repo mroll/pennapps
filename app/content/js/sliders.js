f37e28ecfb2fde09afc61dddb2ae1c6091cb85a3
akw54 <akw54@drexel.edu>
2015-09-05 13:10:02 -0400

    ajax

--- app/content/js/sliders.js
+++ app/content/js/sliders.js
@@ -1,13 +1,11 @@
 var slidersArray = ["k-slider", "r-slider", "v-slider", "t-slider"];
 for (var i = 0; i < slidersArray.length; i++) {
     $("#slider-container").append("<div class='col-md-1 clearfix' style='width: 12%; margin-bottom: 10px;'><input id=" +
-        slidersArray[i] + " type='text' /></div>")
-    ;
+        slidersArray[i] + " type='text' /></div>");
 }
 
 $("#k-slider").slider({
     id: "dataTrack",
-    change: changeFunc(),
     min: 0,
     max: 10,
     value: 0,
@@ -17,6 +15,8 @@ $("#k-slider").slider({
         return 'Current value: ' + value;
     },
     reversed: true
+}).on('slideStop', function (ev) {
+    changeFunc();
 });
 $("#r-slider").slider({
     id: "dataTrack",
@@ -55,14 +55,15 @@ $("#t-slider").slider({
     reversed: true
 });
 
-function changeFunc(value){
-    var xmlhttp;
-
-    if(window.XMLHttpRequest){
-
-        xmlhttp=new XMLHttpRequest();
-    }
-
+function changeFunc() {
+    $.ajax({
+            method: "POST",
+            url: "server/bs.tcl",
+            data: $("#k-slider").slider('getValue')
+        })
+        .done(function (msg) {
+            alert("Data Saved: " + msg);
+        });
 }
 
 //<br /><h5><span class='label label-default'>New</span></h5>
