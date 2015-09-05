bd1fc45b389e30fb94c3d1d56a9afaa7ba2ed7e8
Adit Gupta <aditgupta1@yahoo.com>
2015-09-05 10:23:45 -0400

    Changes to sliders.js file. AJAX stuff added

--- app/content/js/sliders.js
+++ app/content/js/sliders.js
@@ -1,11 +1,13 @@
 var slidersArray = ["k-slider", "r-slider", "v-slider", "t-slider"];
 for (var i = 0; i < slidersArray.length; i++) {
     $("#slider-container").append("<div class='col-md-1 clearfix' style='width: 12%; margin-bottom: 10px;'><input id=" +
-        slidersArray[i] + " type='text' /></div>");
+        slidersArray[i] + " type='text' /></div>")
+    ;
 }
 
 $("#k-slider").slider({
     id: "dataTrack",
+    change: changeFunc(),
     min: 0,
     max: 10,
     value: 0,
@@ -52,5 +54,15 @@ $("#t-slider").slider({
     },
     reversed: true
 });
+
+function changeFunc(value){
+    var xmlhttp;
+
+    if(window.XMLHttpRequest){
+
+        xmlhttp=new XMLHttpRequest();
+    }
+
+}
 
 //<br /><h5><span class='label label-default'>New</span></h5>
